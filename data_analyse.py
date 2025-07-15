513ec4648ebbae2def71eb4267613dc35ec4a203
Yao Trezise <yao.trezise@gmail.com>
2025-07-15 15:06:46 +0200

    log

--- data_analyse.py
+++ data_analyse.py
@@ -1,6 +1,21 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-from typing import Literal
+from plotly.subplots import make_subplots
+import seaborn as sns
+import statsmodels.api as sm
+from statsmodels.discrete.discrete_model import BinaryResultsWrapper
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.stats.diagnostic import het_white
+from typing import Literal, List, Optional, Union, Tuple
+from datetime import datetime
+import time
+
+#-----------------------------------------------------------------------------
+# ANALYSE EXPLORATOIRE DES DONNEES
+# ----------------------------------------------------------------------------
 
 #IMPORT DU DATASET DE L INDEX DE QUALITE DE L AIR DE 6 VILLES
 aqi_data = pd.read_csv("Air_Quality.csv", header=0, index_col=0)
@@ -45,7 +60,9 @@ data_city = aqi_data["City"].unique()
 #VALEUR MOYENNE POUR CHAQUE VARIABLE PAR VILLE
 city_mean = aqi_data.groupby("City").mean(numeric_only=True)
 
-def boxplot(col:Literal['CO', 'CO2', 'NO2', 'SO2', 'O3', 'PM2.5', "PM10", "AQI"]) -> go.Figure:
+def boxplot(
+        col:Literal['CO', 'CO2', 'NO2', 'SO2', 'O3', 'PM2.5', "PM10", "AQI"]
+    ) -> go.Figure:
 
     fig = go.Figure()
 
@@ -71,14 +88,55 @@ def boxplot(col:Literal['CO', 'CO2', 'NO2', 'SO2', 'O3', 'PM2.5', "PM10", "AQI"]
 
     return fig
 
+def data_filter(col=None, city=None):
+
+    data = aqi_data.copy()
+    data.index = pd.to_datetime(data.index)
+
+    if col:
+
+        for polluant in col:
+
+            polluant = polluant.split(" ",1)[1]
+            data.drop(columns=polluant,inplace=True)
+
+    if city:
+
+        data = data[data["City"] == city]
+    
+    #DROP VARIABLE CATEGORIELLE
+    drop_col = [col for col in ["City", "Mois"] if col in data.columns]
+    data.drop(columns=drop_col,inplace=True)
+
+    #80% DE VALEUR NaN 
+    data.drop(columns=["CO2"],inplace=True)
+
+    return data
+
+#DISPERTION ET DISTRIBUTION POUR CHAQUE VARIABLE
+def disp_distr(col) -> go.Figure:
+
+    data = data_filter(col)
+
+    plt.figure(figsize=(12,6))
+    sns.pairplot(data,corner=True,plot_kws={"s":2})
+    
+    return plt.gcf()
+
 #MATRICE DE CORRELATION DES VARIABLES PAR VILLE
-def matrice(city:Literal['Brasilia', 'Cairo', 'Dubai', 'London', 'New York', 'Sydney']=None) -> go.Figure:
+def matrice(
+        city:Literal[
+            'Brasilia', 'Cairo', 'Dubai', 'London', 'New York', 'Sydney'
+        ]=None
+    ) -> go.Figure:
 
     drop_col = [col for col in ["City", "Mois"] if col in aqi_data.columns]
 
     if city:
 
-        corr = aqi_data[aqi_data["City"] == city].drop(columns=drop_col).corr()
+        corr = aqi_data[aqi_data["City"] == city].drop(
+            columns=drop_col
+        ).corr()
 
     else :
 
@@ -102,3 +160,223 @@ def matrice(city:Literal['Brasilia', 'Cairo', 'Dubai', 'London', 'New York', 'Sy
 aqi_data.index = pd.to_datetime(aqi_data.index)
 aqi_data["Mois"] = aqi_data.index.month_name()
 mounth_mean = aqi_data.groupby("Mois").mean(numeric_only=True)
+aqi_data.drop(columns=["Mois"],inplace=True)
+
+# ----------------------------------------------------------------------------
+# EXPLICATION DE L AQI
+# ----------------------------------------------------------------------------
+
+polluants = Literal[
+    "Sans PM10", "Sans PM2.5", "Sans NO2", "Sans CO", "Sans O3", "Sans SO2"
+]
+
+villes = Literal[
+    'Brasilia', 'Cairo', 'Dubai', 'London', 'New York', 'Sydney'
+]
+
+# VIF
+def vif_aqi_variable(
+        col:Optional[List[polluants]]=None,
+        city:Optional[villes]=None
+    ) -> pd.DataFrame:
+
+    data = data_filter(col, city)
+    
+    X = data.iloc[:,:-1]
+    X_const = sm.add_constant(X)
+
+    vif = pd.DataFrame({
+        "variable": X_const.columns,
+        "VIF": [variance_inflation_factor(X_const.values, i)
+                for i in range(X_const.shape[1])]
+    })
+
+    return vif
+
+#REGRESSION LINEAIRE
+def reg_line_aqi(
+        col:Optional[List[polluants]]=None,
+        city:Optional[villes]=None,
+    ) -> sm.regression.linear_model.RegressionResultsWrapper:
+
+    data = data_filter(col, city)
+
+    X = data.iloc[:,:-1]
+    X = sm.add_constant(X)
+    y = data.iloc[:,-1]
+
+    model = sm.OLS(y, X).fit()
+
+    return model
+
+#HYPOTHESES
+def reg_hypothese(
+        model:sm.regression.linear_model.RegressionResultsWrapper,
+        get:Literal["resid_hist", "acf","resid_homo","het_white"]
+    ) -> Union[go.Figure, plt.Figure] :
+    
+    residuals = model.resid
+    fitted = model.fittedvalues
+
+    if get == "resid_hist":
+
+        plt.figure(figsize=(12,6))
+        x = np.linspace(residuals.min(), residuals.max(), 100)
+        mu = residuals.mean()
+        sigma = residuals.std()
+        y = (
+            (1 / (sigma * np.sqrt(2 * np.pi))) * 
+            np.exp(-((x - mu) ** 2) / (2 * sigma ** 2))
+        )
+        sns.histplot(x=residuals,stat="density",kde=True,bins=100)
+        plt.plot(x, y, "--",label="Densité normale théorique",color="red")
+        plt.xlabel("")
+        plt.legend()
+
+        return plt.gcf()
+    
+    if get == "acf":
+
+        plot_acf(residuals, lags=40)
+        plt.title("")
+
+        return plt.gcf()
+    
+    if get == "resid_homo":
+
+        plt.figure(figsize=(12,6))
+        plt.scatter(x=fitted,y=residuals)
+        plt.axhline(0, color='red', linestyle='--', linewidth=1)
+
+        return plt.gcf()
+
+    if get == "het_white":
+
+        class Het():
+
+            def __init__(self):
+
+                exog = model.model.exog
+                white_test = het_white(residuals, exog)
+                stat, p_value, f_stat, f_p_value = white_test
+
+                self.stat = stat
+                self.p_value = p_value
+                self.f_stat = f_stat
+                self.f_p_value = f_p_value
+
+        het0 = Het()
+
+        return het0
+    
+# ----------------------------------------------------------------------------
+# Explication de PM2.5
+# ----------------------------------------------------------------------------
+
+map_target_threshold = {    "PM10":40, "PM2.5":20, "NO2":90, 
+                            "O3":100, "SO2":200                 }
+
+map_temporal = {    "Aucune"        :   None, 
+                    "Saisonière"    :   "Season",
+                    "Mensuelle"     :   "Month",
+                    "Journalière"   :   "Day"   }
+j_30 = pd.Timedelta(days=30)
+
+def get_season_num(date:datetime) -> int:
+    month = date.month
+    day = date.day
+    if (month == 12 and day >= 21) or (1 <= month <= 2) or (month == 3 and day < 20):
+        return 1  # Winter
+    elif (month == 3 and day >= 20) or (4 <= month <= 5) or (month == 6 and day < 21):
+        return 2  # Spring
+    elif (month == 6 and day >= 21) or (7 <= month <= 8) or (month == 9 and day < 22):
+        return 3  # Summer
+    else:
+        return 4  # Autumn
+
+
+#REGRESSION LOGISTIQUE
+def reg_log(
+        target:polluants,
+        start:datetime,
+        end:datetime,
+        col:Optional[List[polluants]]=None,
+        city:Optional[villes]=None,
+        binaire:Optional[bool]=None,
+        temp:Optional[Literal["Season","Month","Day"]]=None,
+        balanced:bool=False
+    ) -> Tuple[ BinaryResultsWrapper, Optional[int], pd.Series, 
+                np.ndarray, np.ndarray  ]:
+
+    target = "Sans " + target
+    col.remove(target) if target in col else col
+    target = target.split(" ",1)[1]
+    start = pd.Timestamp(start).tz_localize("UTC")
+    end = pd.Timestamp(end).tz_localize("UTC")
+
+    def set_model(binaire:Optional[bool]=None) -> Tuple[
+        BinaryResultsWrapper, Optional[int], np.ndarray, np.ndarray
+    ]:
+        threshold = None if binaire == (None or False) else 40
+        list_threshold = [20, 40, 50, 100, 150]
+        y_threshold = map_target_threshold[target] 
+        e = True
+        x = -1
+        while e == True:
+            data = data_filter(col, city)
+            mask = (data.index>=start) & (data.index<=end)
+            data = data.loc[mask]
+            if temp:
+                if temp == "Season":
+                    data["Season"] = [get_season_num(d) for d in data.index]
+                elif temp == "Month":
+                    data["Month"] = data.index.month
+                else:
+                    data["Day"] = data.index.weekday
+                data = pd.get_dummies(  data=data, columns=[temp],
+                                        drop_first=True ).astype(float)
+            if balanced:
+                data[target] = np.where(data[target]>y_threshold, 0, 1)
+                if len(data) > 10000:
+                    count = data[target].value_counts().min()
+                    replace = False
+                else:
+                    count = data[target].value_counts().max()
+                    replace = True
+                data = (
+                    data.groupby(target)
+                    .apply(
+                        lambda x: x.sample( n=count, random_state=100,
+                                            replace=replace )
+                        )
+                    .reset_index(drop=True)
+                )
+            X = data.copy()
+            X = X.drop(columns=["AQI", target])
+            try:
+                if binaire:
+                    if "PM10" in X.columns and binaire==True:
+                        if x<= 4 :
+                            threshold = (
+                                list_threshold[x] if x >= 0 else threshold
+                            )
+                        else:
+                            threshold -= 1
+                        X["PM10"] = np.where(X["PM10"]>threshold, 0, 1)
+                X = sm.add_constant(X, has_constant="add")
+                y = data.loc[:,target]
+                if not balanced:
+                    y = np.where(y>y_threshold, 0, 1)
+                class_0 = y[y==0]
+                class_1 = y[y==1]
+                model = sm.Logit(y, X).fit()
+                e = False
+            except :
+                x += 1
+                continue
+
+        return model, threshold, class_0, class_1
+    
+    model, threshold, class_0, class_1 = set_model(binaire)
+
+    return model, threshold, np.exp(model.params), class_0, class_1
--- presentation_app.py
+++ presentation_app.py
@@ -1,18 +1,21 @@
 import streamlit as st
 import data_analyse as d_a
+from typing import Literal, Tuple, List
 
-URL = "https://www.kaggle.com/datasets/youssefelebiary/air-quality-2024?resource=download"
+URL = """https://www.kaggle.com/datasets/youssefelebiary/
+air-quality-2024?resource=download"""
 
 def exploration():
 
-    st.markdown("### :material/analytics: Analyse Exploratoire des Données")
+    st.markdown(    "### :material/data_exploration: "
+                    "Analyse Exploratoire des Données"  )
 
     st.markdown("##### Dataset Global Air Quality (2024) - 6 Cities")
     st.dataframe(d_a.aqi_data)
     st.info(
-        f"""**Nonbre de ligne** : {d_a.aqi_data_shape[0]} \n
-**Nombre de colonne** : {d_a.aqi_data_shape[1]} \n
-**Nombre de doublon** : {d_a.nb_doublon}"""
+        f"**Nonbre de ligne** : {d_a.aqi_data_shape[0]}"
+        f"\n\n**Nombre de colonne** : {d_a.aqi_data_shape[1]}"
+        f"\n\n**Nombre de doublon** : {d_a.nb_doublon}"
     )
     st.link_button(label="En voir plus", url=URL)
 
@@ -33,28 +36,199 @@ def exploration():
     )
     st.plotly_chart(d_a.boxplot(selected_col))
 
+    st.markdown("### Pairplots")
+    sup_col = st.multiselect(
+        label="Sélectionner une/des variable(s) à supprimer",
+        options=[
+            "Sans PM10", "Sans PM2.5", 
+            "Sans NO2", "Sans CO", 
+            "Sans O3", "Sans SO2"
+        ],
+        max_selections=5,
+        default=["Sans PM10", "Sans NO2", "Sans CO","Sans SO2", "Sans O3"]
+    )
+ 
+    st.pyplot(d_a.disp_distr(sup_col),use_container_width=True)
+
     st.markdown("##### Matrice de corrélation des variables")
     selected_city = st.selectbox(
         label="Sélectionner une ville",
-        options=["Aucune",'Brasilia', 'Cairo', 'Dubai', 'London', 'New York', 'Sydney']
+        options=[
+            "Aucune",'Brasilia',
+            'Cairo', 'Dubai',
+            'London', 'New York',
+            'Sydney'
+            ]
     )
     selected_city = None if selected_city == "Aucune" else selected_city
     st.plotly_chart(d_a.matrice(selected_city))
 
+def vif_presentation(presentation:Literal[1,2]) -> Tuple[List[str],str]:
+
+    col1, col2 = st.columns(2)
+
+    options = [
+        "Sans PM10", "Sans PM2.5", "Sans NO2", 
+        "Sans CO", "Sans O3", "Sans SO2"
+    ]
+    default = "Sans PM10"
+    max_selections = 5
+
+    with col1:
+
+        del_col = st.multiselect(
+            label="Sélectionner une/des variable(s) à supprimer",
+            options=options,
+            max_selections=max_selections,
+            default=default
+        )
+
+    with col2:
+
+        city = st.selectbox(
+            label="   ",
+            options=[
+                "Toutes les villes", "Brasilia",
+                'Cairo', 'Dubai', 'London', 
+                'New York', 'Sydney'
+            ],
+        )
+    
+    st.markdown("##### Facteur d'inflation de la variance")
+
+    city = None if city == "Toutes les villes" else city
+
+    vif = d_a.vif_aqi_variable(del_col,city)
+    st.dataframe(vif)
+
+    return del_col, city
+
+
+def prevision():
+
+    st.markdown("### :material/straighten: Prédiction de l'AQI")
+
+    del_col, city = vif_presentation(1)
+
+    st.markdown("##### Régression linéaire")
+    model = d_a.reg_line_aqi(del_col, city)
+    st.write(model.summary())
+    if st.toggle(
+        label=":blue-badge[:material/info: Afficher l'analyse des résidus]"
+        ):
+        st.markdown("##### Histogramme des résidus")
+        st.pyplot(d_a.reg_hypothese(model,"resid_hist"))
+        st.info(f"**Moyenne des résidus :** {model.resid.mean()}")
+        st.markdown("##### Autocorrélation des résidus")
+        st.pyplot(d_a.reg_hypothese(model,"acf"))
+        st.markdown("##### Homoscédasticité")
+        het0 = d_a.reg_hypothese(model,"het_white")
+        st.info(
+            f"**stat** : {het0.stat}\n\n"
+            f"**p_value** : {het0.p_value}\n\n"
+            f"**f_stat** : {het0.f_stat}\n\n"
+            f"**f_p_value** : {het0.f_p_value}\n\n"
+        )
+        st.pyplot(d_a.reg_hypothese(model,"resid_homo"))
+    st.info(
+        "**Linéarité :** ✓ \n\n"
+        "**Distribution normale des résidus :** X \n\n"
+        "**Moyenne des résidus nulles :** ✓ \n\n"
+        "**Non autocorrélation des résidus :** X\n\n"
+        "**Homoscédasticité :** X"
+    )
 
 def explication():
 
-    st.markdown("")
+    st.markdown("### :material/search: Explication de PM2.5")
+    target = st.segmented_control(  label="Choisir la target",
+                                    options=d_a.map_target_threshold.keys(),
+                                    selection_mode="single",
+                                    default="PM2.5"   )
+    col1, col2 = st.columns(2)
+    with col1:
+        start = st.date_input(  label="Date de début",
+                                value=d_a.aqi_data.index.min(),
+                                min_value=d_a.aqi_data.index.min(),
+                                max_value=d_a.aqi_data.index.max()-d_a.j_30   )
+    with col2:
+        end = st.date_input(    label="Date de fin",
+                                value=d_a.aqi_data.index.max(),
+                                min_value=start+d_a.j_30,
+                                max_value=d_a.aqi_data.index.max()    )
+    del_col, city = vif_presentation(2)
+    st.markdown("##### Régression logistique")
+    binaire = None
+    def show_treshold(threshold:int):
+
+        st.badge(  
+            color="blue", 
+            icon=":material/info:",
+            label=  f"Threshold = {threshold}, "
+                    f"if '{target}' > {threshold}, '{target}' = 0 else 1"
+        )
+
+    temporal = st.selectbox(    label="Ajout d'une variable temporelle",
+                                options=[   "Aucune",
+                                            "Saisonière", 
+                                            "Mensuelle",
+                                            "Journalière"    ],
+                                placeholder="Aucune"    )
+    temporal = d_a.map_temporal[temporal]
+    if ("Sans PM10" not in del_col) and (target != "PM10"):
+        binaire = st.selectbox( 
+            label="Transformer la variable PM10 en binaire",
+            options=["False","True"],
+            placeholder="False"
+        )
+        binaire = True if binaire == "True" else False
+    balanced =  st.toggle(label="Équilibrer les classes")
+    model, threshold, model_params, c0, c1 = d_a.reg_log(   target=target,
+                                                            start=start,
+                                                            end=end,
+                                                            col=del_col,
+                                                            city=city,
+                                                            binaire=binaire,
+                                                            temp=temporal,
+                                                            balanced=balanced)
+    if binaire == True:
+        show_treshold(threshold)
+    
+    st.write(model.summary())
+    st.warning( f"**Nombre de valeur dans la classe 0 :** {len(c0)}\n\n"
+                f"**Nombre de valeur dans la classe 1 :** {len(c1)}"    )
+    st.dataframe(model_params)
+
 
 def ouverture():
-
-    st.markdown("")
+    
+    st.markdown(
+        "### :material/euro_symbol: "
+        "Impact de l'activité économique sur l'AQI"
+    )
 
 pages = {
     "Pages":[
-        st.Page(page=exploration,title="Exploration des données",icon=":material/analytics:"),
-        st.Page(page=explication,title="Explication de l'AQI",icon=":material/bar_chart_4_bars:"),
-        st.Page(page=ouverture,title="Ouverture",icon=":material/euro_symbol:"),
+        st.Page(
+            page=exploration,
+            title="Exploration des données",
+            icon=":material/data_exploration:"
+        ),
+        st.Page(
+            page=prevision,
+            title="Prédiction de l'AQI",
+            icon=":material/straighten:"
+        ),
+        st.Page(
+            page=explication,
+            title="Explication de PM2.5",
+            icon=":material/search:"
+        ),
+        st.Page(
+            page=ouverture,
+            title="Ouverture",
+            icon=":material/euro_symbol:"
+        ),
     ]
 }
 
